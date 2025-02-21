942eb511801160327890c1206a4fc073edf3c0bb
iankalcevic <iankalcevic@usf.edu>
2025-02-21 13:38:41 -0500

    Task 3 - Created Company Class

--- cc_9.js
+++ cc_9.js
@@ -37,3 +37,22 @@ const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
 console.log(mgr1.getDetails()); //Expected output: "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
 
 console.log(mgr1.calculateBonus()); //Expected output: 9600
+
+// Task 3 - Creating a Company Class
+class Company {
+    constructor(name) {
+        this.name = name; //Company name
+        this.employees = [] //Company employees
+    }; //constructor
+    addEmployee(employee) {
+        this.employees.push(employee); //Add employees to company
+    };
+};
+
+const company = new Company("TechCorp");
+company.addEmployee(emp1);
+company.addEmployee(mgr1);
+company.listEmployees();
+//Expected output: 
+//"Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
+//"Manager: John Smith, ID: 201, Department: IT, Salary, $8000, Team Size: 5"
